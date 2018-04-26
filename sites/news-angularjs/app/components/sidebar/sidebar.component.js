b958380fcafa2a79bba15f3181e44dd9f5da23e9
MihirManek <mihir.manek@somaiya.edu>
2018-04-26 12:08:14 +0530

    renamed file to avoid 404 errors

--- sites/news-angularjs/app/components/sidebar/sidebar.component.js
+++ sites/news-angularjs/app/components/sidebar/sidebar.component.js
@@ -2,5 +2,5 @@
  * Created by Jarvis on 1/26/2018.
  */
 app.component('sidebar', {
-    templateUrl: 'app/components/sidebar/_sidebar.html',
+    templateUrl: 'app/components/sidebar/sidebar.html',
 });
